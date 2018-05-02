049afbe840e46ed375b04a0285c790fdd27ba3c4
britney.diboll <britney.diboll@gmail.com>
2018-05-02 15:50:59 -0400

    Login works, working on signup

--- VSCode/public/main.js
+++ VSCode/public/main.js
@@ -1,36 +1,62 @@
-angular
-  .module("main", [])
-  .controller("mainController",["$scope", "$http", ($scope, $http) => {
+const app = angular.module("main", ['ngRoute', 'LocalStorageModule']);
 
-    $scope.login = () => {
+app.controller("mainController", ["$scope", "$http", ($scope, $http) => {
 
-      const data ={
-        username: $scope.username,
-        password: $scope.password,
-        grant_type: "password"
-      }
+  $scope.login = () => {
 
-      // to post a person
-
-      $http({
-        method:"POST",
-        url: "http://localhost:54207/token",
-        data: data,
-        headers:{
-          "Content-Type":"application/x-www-form-urlencoded"
-        },
-        transformRequest: function(obj) {
-          var str = [];
-          for(var p in obj)
-          str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-          return str.join("&");
-       },
-      }).then(resp => {
-        console.log(resp)
-        // store the token
-        if (resp.status == 200){
-          localStorage.setItem("token", resp.data.access_token);
-        }
-      })
+    const data = {
+      username: $scope.username,
+      password: $scope.password,
+      grant_type: "password"
     }
-  }])
+
+    // to post a person
+
+    $http({
+      method: "POST",
+      url: "http://localhost:54207/token",
+      data: data,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      transformRequest: function (obj) {
+        var str = [];
+        for (var p in obj)
+          str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+        return str.join("&");
+      },
+    }).then(resp => {
+      console.log(resp)
+      // store the token
+      if (resp.status == 200) {
+        localStorage.setItem("token", resp.data.access_token);
+      }
+    })
+  }
+
+
+}]);
+
+app.controller('signupController', ['$scope', '$http', function ($scope, $http) {
+
+  $scope.message = "";
+
+  $scope.registration = {
+    userName: "",
+    password: "",
+    confirmPassword: ""
+  };
+
+  $scope.signUp = function () {
+
+    // the sign up post
+    // the login post
+
+    var startTimer = function () {
+      var timer = $timeout(function () {
+        $timeout.cancel(timer);
+        $location.path('/login');
+      }, 2000);
+    }
+  }
+}]);
