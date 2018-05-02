ffed52542bb9d20207e6567256424564b116b851
britney.diboll <britney.diboll@gmail.com>
2018-05-02 15:02:07 -0400

    Auth working & frontend can now get access token

--- VSCode/public/main.js
+++ VSCode/public/main.js
@@ -1,5 +1,36 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
-}
+angular
+  .module("main", [])
+  .controller("mainController",["$scope", "$http", ($scope, $http) => {
 
-document.addEventListener('DOMContentLoaded', main)
+    $scope.login = () => {
+
+      const data ={
+        username: $scope.username,
+        password: $scope.password,
+        grant_type: "password"
+      }
+
+      // to post a person
+
+      $http({
+        method:"POST",
+        url: "http://localhost:54207/token",
+        data: data,
+        headers:{
+          "Content-Type":"application/x-www-form-urlencoded"
+        },
+        transformRequest: function(obj) {
+          var str = [];
+          for(var p in obj)
+          str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+          return str.join("&");
+       },
+      }).then(resp => {
+        console.log(resp)
+        // store the token
+        if (resp.status == 200){
+          localStorage.setItem("token", resp.data.access_token);
+        }
+      })
+    }
+  }])
