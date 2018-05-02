2debd3907624e9dd4877e535ad0bba7e2514f3c1
Charles Bayley <cbayley95@gmail.com>
2018-05-01 21:24:44 -0400

    Added OAuth configuration to startup

--- OAuthTutorial/Startup.cs
+++ OAuthTutorial/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
 using Owin;
 using System;
 using System.Collections.Generic;
@@ -12,9 +13,27 @@ namespace OAuthTutorial
     {
         public void Configuration(IAppBuilder app)
         {
+            ConfigureOAuth(app);
+
             HttpConfiguration config = new HttpConfiguration();
             WebApiConfig.Register(config);
             app.UseWebApi(config);
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
         }
     }
 }
